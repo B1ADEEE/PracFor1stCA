0bcfb7bf67913e3eab3bbbb9a1eec8b3243a6f45
Joshua Crowley Nealon <S00198699@mail.itsligo.ie>
2020-02-07 10:00:36 +0000

    Add project files.

---
+++ PracFor1stCA/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace PracFor1stCA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("are you male or female? ");
+            string gender = Console.ReadLine();
+            Console.WriteLine("what are there ages");
+            int ages = int.Parse(Console.ReadLine());
+            int calQuote = 0;
+            int printSta = 0;
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Calculate Quote");
+            Console.WriteLine("2) Print Statistic");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    calQuote = 1;
+                    break;
+                case "2":
+                    printSta = 1;
+                    break;
+                case "3":
+                    
+                    break;
+                default:
+                    break;
+            }
+            while (calQuote == 1 || printSta == 1)
+            {
+                if (calQuote == 1)
+                {
+
+                }
+                if (printSta == 1)
+                {
+
+                }
+            }
+            if ()
+            
+        }
+    }
+}
